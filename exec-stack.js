0401002ceb4f073627d56483377028d46af2b2e7
velveet <velveet@pm.me>
2020-12-20 09:18:38 -0500

    Execution Stack Process.

---
+++ exec-stack.js
@@ -0,0 +1,19 @@
+
+let firstName = 'john';
+
+console.log(firstName);
+
+function a() {
+    console.log('hello');
+    b();
+}
+
+function b() {
+    console.log('hi');
+}
+
+a();
+
+let lastName = 'smith';
+
+console.log(lastName);
